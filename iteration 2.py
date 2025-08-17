fb5d93dc55e2f54ee2adfb0d6f3c6f551d0417f7
factsbroicl <kevin.fong@my.bdsc.school.nz>
2025-08-18 08:53:37 +1200

    Update iteration 2.py

--- iteration 2.py
+++ iteration 2.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import scrolledtext
+from tkinter import ttk, scrolledtext
 import json
 from os import path
 import bcrypt
@@ -13,9 +12,11 @@ file_path = path.abspath(__file__)
 dir_path = path.dirname(file_path)
 data_path = path.join(dir_path, "data.json") #absolute filepath
 login_path = path.join(dir_path, "login.json")
+log_path = path.join(dir_path, "logs.json")
 
 data = {} #Stores data
 login = {}
+logs = {}
 user = 0
 
 class container(tk.Tk): #Container class for frames
@@ -37,14 +38,62 @@ class container(tk.Tk): #Container class for frames
     
     def show(self, page): #Function to show frames
         frame = self.frames[page]
-        self.refresh(page) #Updates the frame on change
         frame.tkraise()
+        if page == "Budget":
+            self.refresh_box_budget()
+            self.refresh_progressbar_budget()
+            self.refresh_log_budget()
     
-    def refresh(self, page): #refresh page
-        frame = self.frames[page]
-        #WIP
+    def refresh_box_budget(self):
+        frame = self.frames["Budget"]
+        frame.update_idletasks()
         try:
+            list = []
+            for i,j in enumerate(data[user]):
+                list.append(j[1])
+            frame.select = ttk.Combobox(frame.div_relative, values=list)
+            frame.select.config(height=5, width=15, state="readonly")
+            frame.select.bind("<<ComboboxSelected>>", lambda e: frame.progress_update())
+            frame.select.set(data[user][0][1])
+            frame.select.grid(row=4, column=2, padx=5, pady=(0, 5), sticky="nsew")
+        except:
+            frame.select = ttk.Combobox(frame.div_relative, values=["Nothing"])
+            frame.select.config(height=5, width=15, state="readonly")
+            frame.select.bind("<<ComboboxSelected>>", lambda e: frame.progress_update())
+            frame.select.grid(row=4, column=2, padx=5, pady=(0, 5), sticky="nsew")
+    
+    def refresh_progressbar_budget(self):
+        frame = self.frames["Budget"]
+        frame.update_idletasks()
+        try:
+            frame.progress = ttk.Progressbar(frame.div_relative, length=320)
+            frame.progress.grid(row=5, column=0, padx=5, pady=(0, 5), sticky="ew", columnspan=4)
+            percent = data[user][index_calculate(frame.select.get(), data[user], 1)][3] / data[user][index_calculate(frame.select.get(), data[user], 1)][2] * 100
+            if percent >= 100:
+                frame.nametext = tk.Label(frame.div_relative, text="Goal: Reached!", anchor="e", justify="right", font=("arial", 10, "bold"))
+                frame.nametext.grid(row=4, column=1, padx=5, pady=(0, 5), sticky="nsew")
+                percent = 99.99
+            frame.progress.step(percent)
+        except:
             pass
+    
+    def refresh_log_budget(self, text):
+        frame = self.frames["Budget"]
+        frame.update_idletasks()
+        try:
+            frame.log.config(state="normal")
+            frame.log.insert(tk.INSERT, text)
+            frame.log.config(state="disabled")
+        except:
+            pass
+    
+    def proc_log(self):
+        frame = self.frames["Budget"]
+        frame.update_idletasks()
+        try:
+            frame.log.config(state="normal")
+            frame.log.insert(tk.INSERT, logs[user])
+            frame.log.config(state="disabled")
         except:
             pass
 
@@ -78,6 +127,7 @@ class Login(tk.Frame): #Add page frame
         global user
         username = self.username.get()
         password = self.password.get()
+        password_c = 0
         try:
             password_c = hash_bytes(copy.deepcopy(login[username]))
             password = bytes(password, "utf-8")
@@ -86,12 +136,16 @@ class Login(tk.Frame): #Add page frame
         try:
             if bcrypt.checkpw(password, password_c): # Password check
                 user = copy.deepcopy(username) # Sets user
-                self.control.show("Budget")
+                self.control.proc_log()
                 password_c = 0
+                self.control.show("Budget")
             else:
                 self.thread("Invalid username or password.")
         except: # Except due to deepcopy of login, if incorrect username, it would return an error
-            self.thread("Invalid username or password.")
+            if password_c == 0: # This will return an error due to self.control.show handling multiple functions
+                pass
+            else:
+                self.thread("Invalid username or password.")
 
     def warning(self, text):
         warning = tk.Text(self.div_relative, width=16, height=4, wrap="word")
@@ -118,7 +172,7 @@ class Sign_up(tk.Frame):
         self.username.grid(row=0, column=0, padx=5, pady=(0, 5), sticky="ew")
 
         self.password = ttk.Entry(self.div_relative)
-        self.password.insert(0, "Username")
+        self.password.insert(0, "Password")
         self.password.bind("<FocusIn>", lambda e: self.password.delete('0', 'end'))
         self.password.grid(row=1, column=0, padx=5, pady=(0, 5), sticky="ew")
 
@@ -165,7 +219,9 @@ class Sign_up(tk.Frame):
                 login.update(dict([tupleconver(temp_login_data)])) # Tuple to be able to store
                 updated(login, login_path)
                 data.update(dict.fromkeys([temp_login_data[0]], [])) # Adds keys with empty values for future use
+                logs.update(dict.fromkeys([temp_login_data[0]], "Logs: "))
                 updated(data, data_path)
+                updated(logs, log_path)
                 temp_login_data = []
                 print("Account created.")
                 self.control.show("Login")
@@ -191,48 +247,88 @@ class Budget(tk.Frame): #Add page frame
         self.div_relative = ttk.LabelFrame(self, text="Budget") #Adds a seperated Div
         self.div_relative.grid(row=0, column=0, padx=20, pady=10)
 
-        spacer1 = tk.Label(self.div_relative, text="", width=10)
-        spacer1.grid(row=0, column=3)
-
         self.goal = ttk.Entry(self.div_relative, width=10)
-        self.goal.insert(0, "Amount")
+        self.goal.insert(0, "Amount in $")
         self.goal.bind("<FocusIn>", lambda e: self.goal.delete('0', 'end'))
         self.goal.grid(row=0, column=1, padx=5, pady=(0, 30), sticky="ew")
 
-        self.contribute = ttk.Entry(self.div_relative, width=10)
-        self.contribute.insert(0, "Amount")
-        self.contribute.bind("<FocusIn>", lambda e: self.contribute.delete('0', 'end'))
-        self.contribute.grid(row=1, column=1, padx=5, pady=(0, 30), sticky="ew")
+        self.name = ttk.Entry(self.div_relative, width=10)
+        self.name.insert(0, "Item name")
+        self.name.bind("<FocusIn>", lambda e: self.name.delete('0', 'end'))
+        self.name.grid(row=1, column=1, padx=5, pady=(0, 30), sticky="ew")
 
-        self.progress = ttk.Progressbar(self.div_relative, length=320)
-        self.progress.grid(row=2, column=0, padx=5, pady=(0, 5), sticky="ew", columnspan=4)
+        self.contribute = ttk.Entry(self.div_relative, width=10)
+        self.contribute.insert(0, "Amount in $")
+        self.contribute.bind("<FocusIn>", lambda e: self.contribute.delete('0', 'end'))
+        self.contribute.grid(row=3, column=1, padx=5, pady=(0, 30), sticky="ew")
 
         percent0 = tk.Label(self.div_relative, text="0%", anchor="w", justify="left", font=("arial", 10, "bold"))
-        percent0.grid(row=3, column=0, padx=5, pady=(0, 5), sticky="nsew")
+        percent0.grid(row=6, column=0, padx=5, pady=(0, 5), sticky="nsew")
 
         percent100 = tk.Label(self.div_relative, text="100%", anchor="e", justify="right", font=("arial", 10, "bold"))
-        percent100.grid(row=3, column=3, padx=5, pady=(0, 5), sticky="nsew")
+        percent100.grid(row=6, column=3, padx=5, pady=(0, 5), sticky="nsew")
 
-        add = tk.Button(self.div_relative, text="Add") # command= 
-        add.grid(row=1, column=0, padx=5, pady=(0, 30), sticky="nsew")
+        add = tk.Button(self.div_relative, text="Add", command=lambda : self.contribute_money(self.select.get()))
+        add.grid(row=3, column=0, padx=5, pady=(0, 30), sticky="nsew")
 
-        set = tk.Button(self.div_relative, text="Set") # command= 
-        set.grid(row=0, column=0, padx=5, pady=(0, 30), sticky="nsew")
+        set = tk.Button(self.div_relative, text="Set", command=lambda : self.set_goal())
+        set.grid(row=0, column=0, padx=5, pady=(0, 30), sticky="nsew", rowspan=2)
+
+        nametext = tk.Label(self.div_relative, text="Goal:", anchor="e", justify="right", font=("arial", 10, "bold"))
+        nametext.grid(row=4, column=1, padx=5, pady=(0, 5), sticky="nsew")
 
         logtext = tk.Label(self.div_relative, text="Logs", anchor="w", justify="left", font=("arial", 10, "bold"))
-        logtext.grid(row=4, column=0, padx=5, pady=(0, 5), sticky="nsew")
+        logtext.grid(row=7, column=0, padx=5, pady=(0, 5), sticky="nsew")
 
         self.log = scrolledtext.ScrolledText(self.div_relative, wrap="word")
         self.log.config(state="disabled", width=40, height=7)
-        self.log.grid(row=5, column=0, padx=5, pady=(0, 30), sticky="nsew", columnspan=4)
-        self.log.insert(tk.INSERT,
-                        """ Test
-                            Test2
-                            Test3
-                        """)
+        self.log.grid(row=8, column=0, padx=5, pady=(0, 30), sticky="nsew", columnspan=4)
 
         change = ttk.Button(self.div_relative, text="View Setlist", command=lambda : control.show("Setlist")) 
-        change.grid(row=6, column=0, padx=5, pady=5, sticky="nsew")
+        change.grid(row=9, column=0, padx=5, pady=5, sticky="nsew")
+    
+    def set_goal(self):
+        total = self.goal.get()
+        name = self.name.get()
+        try:
+            if total[0] == "$":
+                total = total[1:]
+            total = float(total)
+            num = 0
+            final_name = copy.deepcopy(name)
+            for i,j in enumerate(data[user]):
+                if name in j[1]:
+                    num += 1
+                    final_name = name + " (" + str(num) + ")"
+            print("Check")
+            data[user].append([len(data[user]), final_name ,total, 0])
+            updated(data, data_path)
+            text = "\n" + "Added new goal: " + str(name)
+            logs[user] = logs[user] + text
+            updated(logs, log_path)
+            self.control.refresh_log_budget(text)
+            self.control.refresh_box_budget()
+        except:
+            pass
+
+    
+    def contribute_money(self, item):
+        add = self.contribute.get()
+        try:
+            add = float(add)
+            data[user][index_calculate(item, data[user], 1)][3] += add
+            updated(data, data_path)
+            text = "\n" + "Added $" + str(add) + " to goal."
+            logs[user] = logs[user] + text
+            updated(logs, log_path)
+            self.control.refresh_log_budget(text)
+            self.control.refresh_progressbar_budget()
+        except:
+            pass
+    
+    def progress_update(self):
+        self.select.focus()
+        self.control.refresh_progressbar_budget()
 
 class Setlist(tk.Frame): #Add page frame
     def __init__(self, parent, control):
@@ -255,10 +351,10 @@ def updated(file, path): #Updates and syncs files
         except: 
             pass
 
-def index_calculate(inputf,list): #Calculates index of item in list
-    """Gets the index based on first item in 2d lists"""
+def index_calculate(inputf,list,ind): #Calculates index of item in list
+    """Gets the index based on selected item in 2d lists"""
     for i, j  in enumerate(list):
-        if inputf == j[0]:
+        if inputf == j[ind]:
             index=i
     try:
         return index
@@ -290,6 +386,13 @@ with open(login_path, "r") as fp: #Opens files with saved data
         login = json.load(fp)
     except:
         pass
+
+with open(log_path, "r") as fp: #Opens files with saved data
+    try:
+        logs = json.load(fp)
+    except:
+        pass
+
 #Runtime
 root = container()
 root.minsize(400,400)
